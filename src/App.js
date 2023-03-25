1ae23fb6a325aabf1123496548d7d3cfd1e94d01
athirakcdev <athirakc20@gmail.com>
2023-03-25 19:08:00 +0530

    feat: create react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import { useState ,useEffect} from "react";
+import CardList from "./components/card-list/card-list.component"
+import SearchBox from "./components/search-box/search-box.component"
 import './App.css';
 
-function App() {
+const App=()=>{
+    const [searchField,setSearchField]=useState("")
+    // const [title,setTitle]=useState("")
+    const [monsters,setMonsters]=useState([])
+    const [filteredMonsters,setFilteredMonsters]=useState([monsters])
+    console.log("render")
+    useEffect(()=>{
+      console.log("useeffect1")
+      console.log("effect fired")
+      fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response)=>response.json())
+      .then((users)=>setMonsters(users))
+    },[])
+
+     useEffect(()=>{
+      console.log("useeffect2")
+      console.log({monsters})
+      const newFilteredMonsters = monsters.filter((monster)=>{
+        return monster.name.toLocaleLowerCase().includes(searchField)   
+    })
+     setFilteredMonsters(newFilteredMonsters)
+     console.log("effect is firing")
+     console.log({newFilteredMonsters})
+     },[monsters,searchField])
+
+    const onSearchChange=(event)=>{
+    const searchFieldString=event.target.value.toLocaleLowerCase()
+    setSearchField(searchFieldString)
+    }
+    // const onTitleChange=(event)=>{
+    //   const searchFieldString=event.target.value.toLocaleLowerCase()
+    //   setTitle(searchFieldString)
+    //   }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+        <h1 className="app-title">Monster</h1>
+        <SearchBox className="monster-search-box" 
+        placeholder="search monsters" 
+        onChangeHandler={onSearchChange}/>
+        {
+          // <br/>
+        // <SearchBox className="title-search-box" 
+        // placeholder="set title" 
+        // onChangeHandler={onTitleChange}/>
+        }
+        <CardList monsters={filteredMonsters}/>
+  
+        </div>
+      );
 }
 
+// class App extends Component {
+//   constructor(){
+//     super()
+//     this.state={
+//        monsters:[],
+//        searchField:""
+//     }
+//   }
+//   componentDidMount(){
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response)=>response.json())
+//     .then((users)=>this.setState(()=>{
+//       return {monsters:users}
+//     },
+//     ()=>{
+//       console.log(this.state)
+//     }))
+//   }
+//   onSearchChange=(event)=>{
+//     console.log(event.target.value)
+//     const searchString=event.target.value.toLocaleLowerCase()
+//     this.setState(()=>{
+//      return {searchField: searchString}
+//     })
+// }
+// render(){
+//   const {monsters,searchField}=this.state
+//   const {onSearchChange}=this
+//   const filteredMonsters = monsters.filter((monster)=>{
+//     return monster.name.toLocaleLowerCase().includes(searchField)
+    
+// })
+// console.log(filteredMonsters)
+//   return (
+//     <div className="App">
+//     <h1 className="app-title">Monster Rodolex</h1>
+//     <SearchBox className="monster-search-box" placeholder="search monsters" onChangeHandler={onSearchChange}/>
+//     <CardList monsters={filteredMonsters}/>
+//     </div>
+//   );
+// }
+ 
+// }
+
 export default App;
+
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,19 @@
+
+import "./card.css"
+const Card = (props)=> {
+   
+        const {name,id,email} = props.monster
+        return(
+        
+            <div className="card-container" key={id}>
+            <img 
+              alt={`monster ${name}`}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+            <h1>{name}</h1>
+            <p>{email}</p>
+            </div>
+        )
+    
+}
+export default Card
